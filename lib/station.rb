a378e6eba19bff2c1d88c44a2f500ed526b574e9
Margo <margo@margonline.co.uk>
2013-09-03 15:53:01 +0100

    station class

--- lib/station.rb
+++ lib/station.rb
@@ -1,19 +1,40 @@
 class Station
 
-  attr_accessor :capacity
+  attr_accessor :bikes, :capacity, :broken_bikes
   attr_reader :max_capacity
 
-  def initialize(max_capacity)
+#need to keep track of number of bikes we have
+#and number of broken bikes
+
+
+  def initialize(bikes)
+    @bikes = bikes
+    
+    @max_capacity = @bikes.length
     @capacity = max_capacity
-    @max_capacity = max_capacity
+    @broken_bikes = 0
   end
 
   def check_bike?(bike)
-    bike.is_broken?
+    store_broken_bike if bike.is_broken?
   end
 
-  def check_station_is_empty?()
-    
+  def store_broken_bike
+    @broken_bikes += 1  
   end
 
+  def has_bikes?
+    @capacity > 0   
+  end
+
+  def check_out
+    if has_bikes? && rentable_capacity > 0 
+      @capacity -= 1 
+      bikes.pop
+    end
+  end
+
+  def rentable_capacity
+    rentable_capacity = @capacity - @broken_bikes
+  end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,19 +2,56 @@ require 'station'
 
 describe 'Station' do
 
-  let(:station) {Station.new(100)}
+  let(:station) {Station.new(%w(bike1 bike2 bike3 bike4))}
 
-  it 'should check out bikes' do
-    expect(station.capacity).to eq 100
-  end
-
-  it 'should check if bike is broken' do
+  it 'should know if bike is broken' do
     bike = double :bike, { 'is_broken?' => false }
     expect(station.check_bike?(bike)).to be_false
   end
 
+  it 'should know if bike is not broken' do
+    bike = double :bike, { 'is_broken?' => true }
+    expect(station.check_bike?(bike)).to be_true
+  end
+
+  it 'should know if it has bikes' do
+    expect(station.has_bikes?).to be_true 
+  end
+
+  it 'should know if it doesn\'t have bikes' do
+    station = Station.new(%w())
+    expect(station.has_bikes?).to be_false
+  end
+
+  it 'should check out bikes' do
+    expect(station.check_out).to eq "bike4"
+  end
+
+  it 'should not check out bikes if it doesn\'t have any' do
+    station = Station.new(%w())
+    expect(station.check_out).to be_false
+  end
+
+  it 'should know its rentable capacity' do
+    station.broken_bikes = %w().count
+    expect(station.rentable_capacity).to eq 50
+  end
+
+  it 'should only check out bikes if it has rentable capacity' do
+
+    expect(station.check_out).to be_true
+  end  
+
+  # it 'should only check out non broken bikes' do
+    
+  #   expect(station.check_out)
+  #   # expect(station.capacity).to eq 100
+  #   # expect()
+  # end
+
   it 'if station is empty' do
     pending
+    expect
   end
 
 end
